f0838137b74ef0af03f1e74703a4d8a6e95b2eb5
SebastianSoto18 <atlasjuanse12@gmail.com>
2022-07-07 18:06:21 -0500

    añadiendo duncionalidad de salida del boton jugar

---
+++ invocarjuego.js
@@ -0,0 +1,10 @@
+var juego = document.querySelector("#jugarb");
+
+juego.addEventListener("click", function(e) {
+    e.preventDefault();
+    juego.classList.add("slide-out-elliptic-top-bck");
+    juego.style.display = "none";
+
+    //TODO hacer funcionalidad del juego 
+    
+});
