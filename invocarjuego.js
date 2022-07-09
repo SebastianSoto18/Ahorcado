52c7197c6b6b5c8d73dbf911fee076af98f08547
SebastianSoto18 <atlasjuanse12@gmail.com>
2022-07-09 15:24:12 -0500

    trabajando donde no era

--- invocarjuego.js
+++ invocarjuego.js
@@ -1,14 +1,5 @@
 var juego = document.querySelector("#jugarb");
-
+var palabra;
 juego.addEventListener("click", function(e) {
-    e.preventDefault();
-    juego.classList.add("slide-out-elliptic-top-bck");
-    
-    setTimeout(function() { 
-        
-        juego.style.display = "none";
-    },1000);
-
-    //TODO hacer funcionalidad del juego 
-
+  
 });
